258ca5d65ac2bf48cb778b613f24517586665d9b
Wanqj <wqj97@126.com>
2017-10-13 00:52:40 +0800

    2017-10-13

--- inference.py
+++ inference.py
@@ -1,18 +1,28 @@
 import tensorflow as tf
+import numpy as np
+import scipy as sp
+from scipy.linalg import toeplitz
 
 
 class Inference(object):
-    def __init__(self, input_tensor, is_train=True):
+    def __init__(self, input_tensor, kwidth=5, stride=2, is_train=True):
         """
         Build model
         :param Tensor input_tensor: input Tensor
+        :param int kwidth: width of down and up convolution (default: 5)
+        :param int stride: stride of down and up convolution
         :param bool is_train: whether train or not (default: True)
         """
-        self.input = input_tensor
+        self.stride = stride
+        self.kwidth = kwidth
+        self.input_tensor = input_tensor
         self.is_train = is_train
 
         # this list store each down_conv output layer shapes , it makes us more convenient to use up_conv function
-        self.layer_shape = [input_tensor.shape.as_list()]
+        self.layer_shape = []
+
+        # this list represent the kernel of each conv and deconv layer
+        self.num_kernel = [16, 32, 256, 512, 1024]
 
     @staticmethod
     def variable_with_weight_decay(name, shape, wd=None):
@@ -30,9 +40,30 @@ class Inference(object):
             tf.add_to_collection('l2_losses', l2_loss)
         return var
 
+    @staticmethod
+    def get_lpc_a(signals, order):
+        """
+        get lpc a
+        :param list signals: contains signals which need to be calculate
+        :param int order: number of signals need to be calculate
+        :return: list
+        """
+        output = []
+        for signal in signals:
+            p = order + 1
+            r = np.zeros(p, signal.dtype)
+            nx = np.min([p, signal.size])
+            x = np.correlate(signal, signal, 'full')
+            r[:nx] = x[signal.size - 1:signal.size + order]
+            phi = np.dot(sp.linalg.inv(toeplitz(r[:-1])), -r[1:])
+            lpc = np.concatenate(([1.], phi))
+            lpca = -lpc[1:][::-1].T
+            output.append(lpca)
+        return output
+
     def down_conv(self, input_tensor, kwidth, num_kernel, stride, wd=None, padding='SAME', name='down_conv'):
         """
-        1D down convolution
+        2D down convolution
         :param Tensor input_tensor: Tensor which will be convolution
         :param int kwidth: width of convolution
         :param int num_kernel: number of convolution out kernels
@@ -43,9 +74,9 @@ class Inference(object):
         :return: Tensor
         """
         input_shape = input_tensor.shape.as_list()  # get the input shape
-        w = self.variable_with_weight_decay('filter_weight', [kwidth, input_shape[-1], num_kernel], wd)
+        w = self.variable_with_weight_decay('filter_weight', [1, kwidth, input_shape[-1], num_kernel], wd)
         b = tf.get_variable('filter_biases', [num_kernel], initializer=tf.constant_initializer([0.1]))
-        conv = tf.nn.conv1d(input_tensor, w, stride=stride, padding=padding)
+        conv = tf.nn.conv2d(input_tensor, w, strides=[1, 1, stride, 1], padding=padding)
         self.layer_shape.append(conv.shape.as_list())  # this line makes our more convenient to use for up_conv function
         return tf.nn.relu(conv + b, name=name)
 
@@ -65,6 +96,49 @@ class Inference(object):
         input_shape = input_tensor.shape.as_list()  # get the input shape
         w = self.variable_with_weight_decay('filter_weight', [1, kwidth, num_kernel, input_shape[-1]], wd)
         b = tf.get_variable('filter_biases', [num_kernel], initializer=tf.constant_initializer([0.1]))
-        deconv = tf.nn.conv2d_transpose(input_tensor, w, strides=[1, stride, 1, 1], output_shape=output_shape,
+        deconv = tf.nn.conv2d_transpose(input_tensor, w, strides=[1, 1, stride, 1], output_shape=output_shape,
                                         padding=padding)
         return tf.nn.relu(deconv + b, name=name)
+
+    def build_ae_model(self):
+        """
+        Build an AutoEncoder model
+        :return: Tensor
+        """
+        with tf.variable_scope('encoder'):
+            # First we need to reshape input tensor into [-1, kwidth]
+            layer = tf.reshape(self.input_tensor, [-1, self.input_tensor.shape.as_list()[-1]])
+            # Second we need to expand the of layer dimension into [batch_size, height, width, channels] to fit 1d
+            # convolution
+            layer = tf.expand_dims(layer, 1)
+            layer = tf.expand_dims(layer, 3)
+            self.layer_shape.append(layer.shape.as_list())
+            print "down convolution: "
+            for index, num_kernel in enumerate(self.num_kernel):
+                with tf.variable_scope("down_conv_{}".format(index)) as scope:
+                    preview_shape = layer.shape
+                    layer = self.down_conv(layer, self.kwidth, num_kernel, self.stride, name=scope.name)
+                    print "{} -> {}".format(preview_shape, layer.shape)
+        with tf.variable_scope('decoder'):
+            print "up convolution"
+            for index, layer_shape in enumerate(self.layer_shape[::-1][1:]):
+                with tf.variable_scope("up_conv_{}".format(index)) as scope:
+                    preview_shape = layer.shape
+                    layer = self.up_conv(layer, self.kwidth, layer_shape[-1], self.stride,
+                                         output_shape=layer_shape, name=scope.name)
+                    print "{} -> {}".format(preview_shape, layer.shape)
+        return tf.squeeze(layer)
+
+
+if __name__ == '__main__':
+    sess = tf.InteractiveSession()
+    inference = Inference(tf.constant(0.1, dtype=tf.float32, shape=[2, 10, 64]))
+    data = inference.build_ae_model()
+    tf.global_variables_initializer().run()
+    data = data.eval()
+    voice_value = tf.constant(0.1, dtype=tf.float32, shape=[20, 64]).eval()
+    voice_value = inference.get_lpc_a(voice_value, 64)
+    print np.shape(voice_value)
+    print np.shape(data)
+    print tf.convert_to_tensor(voice_value)
+    # print tf.matmul(tf.constant(voice_value), data).eval()
--- reader.py
+++ reader.py
@@ -3,27 +3,32 @@ import os
 
 
 class Reader(object):
-    def __init__(self, path, canvas_size, batch_size, threads=8, pattem='*.tfrecords', preemph=0.):
+    def __init__(self, path, canvas_size, batch_size, window_size=64, threads=8, pattem='*.tfrecords', pre_emph=0.):
         """
         This is Reader
-        :param path: datasets path
-        :param pattem: file pattem (default: *.tfrecords)
-        :param preemph: Pre-emphasis weight (default: 0)
+        :param str path: datasets path
+        :param int canvas_size: canvas_size for each tfrecord line
+        :param int batch_size: number of batch size for each train step
+        :param int window_size: slice each canvas into parts
+        :param int threads: number of read threads
+        :param str pattem: file pattem (default: *.tfrecords)
+        :param pre_emph: Pre-emphasis weight (default: 0)
         """
-        self.preemph = preemph
+        self.kwidth = window_size
+        self.pre_emph = pre_emph
         self.canvas_size = canvas_size
         self.batch_size = batch_size
-        self.theads = threads
+        self.threads = threads
         self.filename_queue = tf.train.string_input_producer(tf.gfile.Glob(os.path.join(path, pattem)))
 
-    @staticmethod
-    def pre_emph(x, coeff=0.95):
-        x0 = tf.reshape(x[0], [1, -1])
-        diff = x[1:] - coeff * x[:-1]
-        concat = tf.cast(tf.concat(0, [x0, diff]), tf.float32)
-        return concat
-
-    def read_and_decode(self, preemph=0.):
+    def get_batch(self):
+        """
+        get train batch
+        reade and decode data.
+        :returns:
+        :var Tensor train_collect: train batch
+        :var Tensor label_collect: label batch
+        """
         reader = tf.TFRecordReader()
 
         _, serialized_example = reader.read(self.filename_queue)
@@ -38,8 +43,32 @@ class Reader(object):
 
         wave = (2. / 65535.) * tf.cast((wave - 32767), tf.float32) + 1.  # normalization
 
-        if preemph > 0:
-            wave = self.pre_emph(wave, preemph)
-        tf.train.shuffle_batch([wave], batch_size=self.batch_size, num_threads=self.theads,
-                               capacity=10 * self.batch_size, min_after_dequeue=1000 + self.batch_size)
-        return wave
+        train_collect = []
+        label_colloect = []
+        for i in xrange(wave.shape[0] - self.kwidth - 1):
+            train_collect.append(wave[i:i + self.kwidth])
+            label_colloect.append([wave[i + self.kwidth]])
+
+        train_collect, label_colloect = tf.train.shuffle_batch([train_collect, label_colloect],
+                                                               batch_size=self.batch_size, num_threads=self.threads,
+                                                               capacity=1000 + 10 * self.batch_size,
+                                                               min_after_dequeue=1000)
+        return train_collect, label_colloect
+
+
+if __name__ == '__main__':
+    """
+    test the batch output
+    train_collect: [batch_size, 2 ** 10 - kwidth, kwidth] -> [1, 959, 64]
+    label_collect: [batch_size, 2 ** 10 - kwidth, 1] -> [1, 959, 1]
+    """
+    import numpy as np
+
+    sess = tf.InteractiveSession()
+    reader = Reader('data', 2 ** 10, 1)
+    batch = reader.get_batch()
+    tf.train.start_queue_runners()
+
+    print np.shape(batch[0])  # print shape of train collect
+    print np.shape(batch[1])  # print shape of label collect
+    print sess.run([batch[0][0][1], batch[1][0][0]])  # test whether we get correct train data and it's label
